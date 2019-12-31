39eeef299dbe82bbee68719d6d37965ea358ad2d
Saber Technologies <33689397+CkMadhavan@users.noreply.github.com>
2019-12-31 13:52:14 +0530

    Create send.py

---
+++ send.py
@@ -0,0 +1,11 @@
+from flask import Flask , render_template,request , redirect , url_for
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    print(os.environ['EMAIL_ADDRESS'])
+   
+if __name__ == "__main__":
+    app.run()

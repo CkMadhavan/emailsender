651cd8a6db00e3b43cd212ca8429581d353c0798
Saber Technologies <33689397+CkMadhavan@users.noreply.github.com>
2019-12-31 18:55:51 +0530

    Update send.py

--- send.py
+++ send.py
@@ -1,11 +1,28 @@
 from flask import Flask , render_template,request , redirect , url_for
 import os
+import smtplib
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return (os.environ['EMAIL_ADDRESS'])
+@app.route('/<to>')
+def index(to):
+    try:
+        
+        email = os.environ['EMAIL_ADDRESS']
+        password = os.environ['PASSWORD']
+        emailto = to
+        server = smtplib.SMTP('smpt.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(email , password)
+        message = 'Subject: {}\n\n{}'.format(subject , msg)
+        server.sendmail(email , to,message)
+        server.quit()
+        
+    except:
+        pass
+    
+    return ''
    
 if __name__ == "__main__":
     app.run()
